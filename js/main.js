b66abf400b87b8a7e4f2c20a64537a8925822108
michellechamp <michellechamp@gmail.com>
2017-12-04 20:49:07 -0500

    add jQuery plug in of falling petals

--- js/main.js
+++ js/main.js
@@ -1,40 +1,9 @@
-$(document).ready(function () {
-    $('#gallery').unitegallery();
-});
-
+// domReady
 $(function () {
-  var $body = $(document.body);
-  var $form = $('#form');
-  var $name = $form.find('#inputName');
-
-  $form.submitter({
-    dataType: 'json',
-    start: function (e) {
-      if (!$name.val()) {
-        e.preventDefault(); // Prevent submit
-        $name.focus();
-
-        // Tooltip: https://github.com/fengyuanchen/tooltip
-        $body.tooltip('Please enter a name', 'warning');
-      }
-    },
-    done: function (e, data) {
-      if ($.isPlainObject(data) && data.success) {
-        $body.tooltip(data.result, 'success');
-      }
-    },
-    fail: function (e, textStatus) {
-      $body.tooltip(textStatus, 'danger');
-    }
-  });
+    $.fn.sakura();
 });
 
-$(document).ready(function () {
-
-    // page is now ready, initialize the calendar...
-
-    $('#calendar').fullCalendar({
-        // put your options and callbacks here
-    })
-
+// windowLoad
+$(window).load(function () {
+    $.fn.sakura();
 });
