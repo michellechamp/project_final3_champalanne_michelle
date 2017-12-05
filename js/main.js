cdd8c5340e6a73f8bb773694aaf51d875a19a3a4
michellechamp <michellechamp@gmail.com>
2017-12-04 20:50:52 -0500

    add textillate.js jQuery plug in

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+//for Textillate.js jQuery plug in: To add effect to "welcome" on index.html
+$(function () {
+    $('.welcometext').textillate({
+        in: { effect: 'rollIn'
+        }
+    });
+});
+
+//Load falling petal jQuery plug in
 // domReady
 $(function () {
     $.fn.sakura();
